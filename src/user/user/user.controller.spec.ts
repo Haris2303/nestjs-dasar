cce1bda76d9d4154a11cdf87f15ed72b611a82c4
haris2303 <hrsccf102@gmail.com>
2024-03-16 11:41:53 +0900

    first

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import * as httpMock from 'node-mocks-http';
-import { title } from 'process';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -9,14 +9,16 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      imports: [],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
   });
 
   it('should can say hello', async () => {
-    const response = await controller.sayHello('Otong', 'Surotong');
-    expect(response).toBe('Hello Otong Surotong');
+    const response = await controller.sayHello('Otong');
+    expect(response).toBe('Hello Otong');
   });
 
   it('should can view template', async () => {
